dae7a90d5b9cf3b49b575fbc8980ce2b9569101e
vlad4433490-design <vlad4433490@gmail.com>
2026-06-24 05:27:37 +0300

    Fix metadata base call

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ function getMetadataBase() {
   }
 }
 export const metadata: Metadata = {
-  metadataBase: getMetadataBase(process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000"),
+  metadataBase: getMetadataBase(),
   title: {
     default: "АЗСТЕХСНАБ - интернет-магазин технических товаров",
     template: "%s | АЗСТЕХСНАБ"
